0555acbf7425145a619fda6eac684874160d1d21
Sandy <51105247+skiesmate@users.noreply.github.com>
2023-04-17 20:23:58 +0530

    Create index.php

---
+++ 3/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="icon" href="./index.ico" type="image/x-icon" />
+  <title>skiesmate</title>
+  <script type="module" crossorigin src="./index.js"></script>
+</head>
+<body>
+  <?php
+    echo '<iframe id="msBingFrame" width="100%" height="100%"> </iframe>';
+  ?>
+</body>
+</html>
